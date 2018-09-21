f7b22c65264c44580f893a284e9cfd7261094789
Christian Veelken <christian.veelken@cern.ch>
2018-09-21 17:21:58 +0300

    added (dummy) analyze samples for HH->bbWW analysis

---
+++ python/samples/hhAnalyzeSamples_2017.py
@@ -0,0 +1,44 @@
+from tthAnalysis.HiggsToTauTau.samples.tthAnalyzeSamples_2017 import samples_2017 as samples_2017_general
+from hhAnalysis.multilepton.samples.hhAnalyzeSamples_2017_hh_private import samples_2017 as samples_2017_hh
+
+import collections
+import itertools
+
+del samples_2017_hh['sum_events']
+samples_2017 = collections.OrderedDict(itertools.chain(
+  samples_2017_general.items(), samples_2017_hh.items()
+))
+
+from collections import OrderedDict as OD
+
+for sample_name, sample_info in samples_2017.items():
+
+  if not isinstance(sample_info, OD):
+    continue
+
+  if sample_name.startswith('/HHTo'):
+    sample_info["use_it"] = True
+  #------------------------------------------------------------------------------
+  # CV: ONLY FOR TESTING !!!
+  else:
+    sample_info["use_it"] = False
+    continue  
+  #------------------------------------------------------------------------------
+
+  if sample_name.startswith('/ZZ'):
+    sample_info["sample_category"] = "ZZ"
+  elif sample_name.startswith('/WZ'):
+    sample_info["sample_category"] = "WZ"
+  elif sample_name.startswith('/WW'):
+    sample_info["sample_category"] = "WW"
+  elif sample_name.startswith('/DY') and sample_name.find('JetsToLL') < 10:
+    sample_info["sample_category"] = "DY"
+  elif sample_name.startswith('/W') and sample_name.find('JetsToLNu') < 10:
+    sample_info["sample_category"] = "W"
+  elif sample_name.startswith('/ttH'):
+    sample_info["sample_category"] = "TTH"
+
+  if sample_info["sample_category"] == "Rares":
+    sample_info["sample_category"] = "Other"
+  if sample_info["sample_category"] in [ "tHq", "tHW" ]:
+    sample_info["sample_category"] = "TH"
