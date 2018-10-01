9a3af82d53a8abce1b626f6cdb4a3e3a196c4c52
Karl Ehatäht <karlehataht@gmail.com>
2018-10-01 12:35:32 +0300

    Add ttH powheg sample back to the list of samples used for BDT training

--- python/samples/hhAnalyzeSamples_2017_BDT.py
+++ python/samples/hhAnalyzeSamples_2017_BDT.py
@@ -4,6 +4,7 @@ for sample_name, sample_info in samples_2017.items():
   if sample_name == 'sum_events':
     continue
   if sample_info["process_name_specific"] in [
+        "ttHToNonbb_M125_powheg",
         "TTZJets_LO",
         "TTWJets_LO",
         "TTTo2L2Nu",
