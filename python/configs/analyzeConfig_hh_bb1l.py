f68bfe0a62ed658dca6f509efaef41661c178782
ktht <karlehataht@gmail.com>
2018-10-31 22:54:56 +0200

    Do not overwrite samples member variable in derived analyzeConfig-s

--- python/configs/analyzeConfig_hh_bb1l.py
+++ python/configs/analyzeConfig_hh_bb1l.py
@@ -88,8 +88,6 @@ class analyzeConfig_hh_bb1l(analyzeConfig_hh):
       template_dir       = os.path.join(os.getenv('CMSSW_BASE'), 'src', 'hhAnalysis', 'bbww', 'test', 'templates')
     )
 
-    self.samples = samples
-
     self.lepton_selections = [ "Tight", "Fakeable" ]
     self.lepton_frWeights = [ "enabled", "disabled" ]
     self.applyFakeRateWeights = applyFakeRateWeights
