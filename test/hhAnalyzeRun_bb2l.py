2a22e43e190e44827336a376411a8b20051a7edf
saswatinandan <snandan@quasar.hep.kbfi.ee>
2018-11-28 17:22:46 +0200

    *****

--- test/hhAnalyzeRun_bb2l.py
+++ test/hhAnalyzeRun_bb2l.py
@@ -111,7 +111,8 @@ elif mode == "forBDTtraining":
       else:
         for sample_binned_value in sample_value:
           dy_binned_samples = list(
-            filter(lambda sample_name: sample_name.startswith('DY'), sample_binned_value['samples']))
+            filter(lambda sample_name: sample_name.startswith('DY'), sample_binned_value['samples'])
+          )
           dy_samples_binned.extend(dy_binned_samples)
 
   for sample_name, sample_info in samples.items():
