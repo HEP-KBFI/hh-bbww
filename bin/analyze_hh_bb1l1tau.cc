45765eaf023bc5291f06b7c705d144ef9960738c
Christian Veelken <christian.veelken@cern.ch>
2018-12-03 13:55:27 +0200

    improved tabbing

--- bin/analyze_hh_bb1l1tau.cc
+++ bin/analyze_hh_bb1l1tau.cc
@@ -373,11 +373,11 @@ int main(int argc, char* argv[])
 
   const double maxAbsEta_lepton = 2.1;
   double minPt_e = -1.;
-  double minPt_mu  = -1.;
+  double minPt_mu = -1.;
   switch(era)
   {
-    case kEra_2016: minPt_e = 25.; minPt_mu  = 20.; break; // AN2016_372_v14:331
-    case kEra_2017: minPt_e = 30.; minPt_mu  = 25.; break;
+    case kEra_2016: minPt_e = 25.; minPt_mu = 20.; break; // AN2016_372_v14:331
+    case kEra_2017: minPt_e = 30.; minPt_mu = 25.; break;
     case kEra_2018: throw cmsException("analyze_hh_bb1l1tau", __LINE__) << "Implement me!";
     default:        throw cmsException("analyze_hh_bb1l1tau", __LINE__) << "Invalid era = " << era;
   }
