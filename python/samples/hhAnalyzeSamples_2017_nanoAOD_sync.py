1c7d94d9f16932a6737faa7eb74cfbd99ea68a60
ktht <karlehataht@gmail.com>
2018-12-24 01:03:07 +0200

    Sample dict for sync Ntuple (before post-processing)

---
+++ python/samples/hhAnalyzeSamples_2017_nanoAOD_sync.py
@@ -0,0 +1,11 @@
+from hhAnalysis.bbww.samples.hhAnalyzeSamples_2017_nanoAOD import samples_2017
+
+sync_key = "/GluGluToRadionToHHTo2B2VTo2L2Nu_M-750_narrow_13TeV-madgraph_correctedcfg/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"
+
+samples_2017[sync_key]['local_paths'][0]['path'] = '/hdfs/local/karl/sync_ntuples/nanoAODproduction/2018Dec24/signal_ggf_spin0_750_hh_2b2v'
+samples_2017[sync_key]['nof_files'] = 1
+samples_2017[sync_key]['nof_tree_events'] = 52000
+
+for sample_key in samples_2017:
+  if sample_key not in [ sync_key, 'sum_events' ]:
+    del samples_2017[sample_key]
