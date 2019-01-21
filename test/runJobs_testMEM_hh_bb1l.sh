30e3d84436ca7a94521b564aa6ad46e32d4350b1
Christian Veelken <christian.veelken@cern.ch>
2019-01-21 16:05:15 +0200

    fixed typo in name of config files and log files

---
+++ test/runJobs_testMEM_hh_bb1l.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+rm -rf testMEM_hh_bb1l_signal_genMatchOpt1_cfg.py
+sed 's/$PROCESS/signal/ ; s/$GENMATCHINGOPTION/1/' templates/testMEM_hh_bb1l_cfg.py > testMEM_hh_bb1l_signal_genMatchOpt1_cfg.py
+rm -rf testMEM_hh_bb1l_signal_genMatchOpt2_cfg.py
+sed 's/$PROCESS/signal/ ; s/$GENMATCHINGOPTION/2/' templates/testMEM_hh_bb1l_cfg.py > testMEM_hh_bb1l_signal_genMatchOpt2_cfg.py
+rm -rf testMEM_hh_bb1l_signal_genMatchOpt3_cfg.py
+sed 's/$PROCESS/signal/ ; s/$GENMATCHINGOPTION/3/' templates/testMEM_hh_bb1l_cfg.py > testMEM_hh_bb1l_signal_genMatchOpt3_cfg.py
+
+rm -rf testMEM_hh_bb1l_background_genMatchOpt1_cfg.py
+sed 's/$PROCESS/background/ ; s/$GENMATCHINGOPTION/1/' templates/testMEM_hh_bb1l_cfg.py > testMEM_hh_bb1l_background_genMatchOpt1_cfg.py
+rm -rf testMEM_hh_bb1l_background_genMatchOpt2_cfg.py
+sed 's/$PROCESS/background/ ; s/$GENMATCHINGOPTION/2/' templates/testMEM_hh_bb1l_cfg.py > testMEM_hh_bb1l_background_genMatchOpt2_cfg.py
+rm -rf testMEM_hh_bb1l_background_genMatchOpt3_cfg.py
+sed 's/$PROCESS/background/ ; s/$GENMATCHINGOPTION/3/' templates/testMEM_hh_bb1l_cfg.py > testMEM_hh_bb1l_background_genMatchOpt3_cfg.py
+
+rm -rf testMEM_hh_bb1l_signal_genMatchOpt1.log
+testMEM_hh_bb1l testMEM_hh_bb1l_signal_genMatchOpt1_cfg.py &> testMEM_hh_bb1l_signal_genMatchOpt1.log &
+rm -rf testMEM_hh_bb1l_signal_genMatchOpt2.log
+testMEM_hh_bb1l testMEM_hh_bb1l_signal_genMatchOpt2_cfg.py &> testMEM_hh_bb1l_signal_genMatchOpt2.log &
+rm -rf testMEM_hh_bb1l_signal_genMatchOpt3.log
+testMEM_hh_bb1l testMEM_hh_bb1l_signal_genMatchOpt3_cfg.py &> testMEM_hh_bb1l_signal_genMatchOpt3.log &
+
+rm -rf testMEM_hh_bb1l_background_genMatchOpt1.log
+testMEM_hh_bb1l testMEM_hh_bb1l_background_genMatchOpt1_cfg.py &> testMEM_hh_bb1l_background_genMatchOpt1.log &
+rm -rf testMEM_hh_bb1l_background_genMatchOpt2.log
+testMEM_hh_bb1l testMEM_hh_bb1l_background_genMatchOpt2_cfg.py &> testMEM_hh_bb1l_background_genMatchOpt2.log &
+rm -rf testMEM_hh_bb1l_background_genMatchOpt3.log
+testMEM_hh_bb1l testMEM_hh_bb1l_background_genMatchOpt3_cfg.py &> testMEM_hh_bb1l_background_genMatchOpt3.log &
+
+#rm -rf testMEM_hh_bb1l_all.root
+#hadd testMEM_hh_bb1l_all.root testMEM_hh_bb1l_signal_genMatchOpt1.root testMEM_hh_bb1l_signal_genMatchOpt2.root testMEM_hh_bb1l_signal_genMatchOpt3.root testMEM_hh_bb1l_background_genMatchOpt1.root testMEM_hh_bb1l_background_genMatchOpt2.root testMEM_hh_bb1l_background_genMatchOpt3.root
+
