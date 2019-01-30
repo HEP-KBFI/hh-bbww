83bd2775edbd2c8a843daedb1b2a2694c685b987
Christian Veelken <christian.veelken@cern.ch>
2019-01-30 11:37:57 +0200

    disable sub-leading backgrounds and reserve TT samples without PSweights for building datacards

--- python/samples/hhAnalyzeSamples_2017_BDT.py
+++ python/samples/hhAnalyzeSamples_2017_BDT.py
@@ -6,20 +6,21 @@ for sample_name, sample_info in samples_2017.items():
   if sample_name == 'sum_events':
     continue
   if sample_info["process_name_specific"] in [
-        "ttHToNonbb_M125_powheg",
-        "TTZJets_LO",
-        "TTWJets_LO",
-        "TTTo2L2Nu",
+        #"ttHToNonbb_M125_powheg",
+        #"TTZJets_LO",
+        #"TTWJets_LO",
+        # CV: TT samples without PSweights reserved for building datacards !!
+        #"TTTo2L2Nu",
         "TTTo2L2Nu_PSweights",
-        "TTToSemiLeptonic",
+        #"TTToSemiLeptonic",
         "TTToSemiLeptonic_PSweights",
-        "TTToHadronic",
+        #"TTToHadronic",
         "TTToHadronic_PSweights",
       ]:
     sample_info["use_it"] = True
   elif sample_info["process_name_specific"].startswith("signal") and 'hh' in sample_info["process_name_specific"]:
     sample_info["use_it"] = 'nonresonant' not in sample_info["process_name_specific"] # temp: enable resonant samples only
-  elif re.match("WZTo3LNu_(0|1|2|3)Jets.*", sample_info["process_name_specific"]):
-    sample_info["use_it"] = True
+  #elif re.match("WZTo3LNu_(0|1|2|3)Jets.*", sample_info["process_name_specific"]):
+  #  sample_info["use_it"] = True
   else:
     sample_info["use_it"] = False
