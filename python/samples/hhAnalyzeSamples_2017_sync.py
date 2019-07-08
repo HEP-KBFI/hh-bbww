402796faf859bf7f8aec6ecbd122555cfa21e2fd
ktht <karlehataht@gmail.com>
2019-07-08 18:07:40 +0300

    Update sync Ntuple

--- python/samples/hhAnalyzeSamples_2017_sync.py
+++ python/samples/hhAnalyzeSamples_2017_sync.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict as OD
 
-# file generated at 2019-07-04 23:54:32 with the following command:
-# create_dictionary.py -m python/samples/metaDict_2017_hh_sync.py -p /hdfs/local/karl/ttHNtupleProduction/2017/2019Jul04_woPresel_nonNom_hh_bbww_sync/ntuples/ -N samples_2017 -E 2017 -o python/samples -g hhAnalyzeSamples_2017_sync.py -M
+# file generated at 2019-07-08 18:07:31 with the following command:
+# create_dictionary.py -m python/samples/metaDict_2017_hh_sync.py -p /hdfs/local/karl/ttHNtupleProduction/2017/2019Jul08_woPresel_nonNom_hh_bbww_sync/ntuples/ -N samples_2017 -E 2017 -o python/samples -g hhAnalyzeSamples_2017_sync.py -M
 
 samples_2017 = OD()
 samples_2017["/GluGluToRadionToHHTo2B2VTo2L2Nu_M-750_narrow_13TeV-madgraph_correctedcfg/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"] = OD([
@@ -18,7 +18,7 @@ samples_2017["/GluGluToRadionToHHTo2B2VTo2L2Nu_M-750_narrow_13TeV-madgraph_corre
   }),
   ("nof_tree_events",                 52000),
   ("nof_db_events",                   200000),
-  ("fsize_local",                     144943870), # 144.94MB, avg file size 144.94MB
+  ("fsize_local",                     144943143), # 144.94MB, avg file size 144.94MB
   ("fsize_db",                        11931037531), # 11.93GB, avg file size 1.08GB
   ("use_it",                          True),
   ("xsection",                        0.026422),
@@ -30,19 +30,22 @@ samples_2017["/GluGluToRadionToHHTo2B2VTo2L2Nu_M-750_narrow_13TeV-madgraph_corre
   ("local_paths",
     [
       OD([
-        ("path",      "/hdfs/local/karl/ttHNtupleProduction/2017/2019Jul04_woPresel_nonNom_hh_bbww_sync/ntuples/signal_ggf_spin0_750_hh_2b2v"),
+        ("path",      "/hdfs/local/karl/ttHNtupleProduction/2017/2019Jul08_woPresel_nonNom_hh_bbww_sync/ntuples/signal_ggf_spin0_750_hh_2b2v"),
         ("selection", "*"),
         ("blacklist", []),
       ]),
     ]
   ),
-  ("missing_from_superset",           [
+  ("missing_completely",           [
     # not computed
   ]),
-  ("missing_hlt_paths",               [
+  ("missing_from_superset",        [
+    # not computed
+  ]),
+  ("missing_hlt_paths",            [
 
   ]),
-  ("hlt_paths",               [
+  ("hlt_paths",                    [
     # not computed
   ]),
 ])
