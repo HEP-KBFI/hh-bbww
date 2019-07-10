83fc134733cf97baab4f99462448761914b9aa1a
ktht <karlehataht@gmail.com>
2019-07-10 17:15:28 +0300

    Add sample dictionaries for 2016 & 2018 sync Ntuples

---
+++ python/samples/hhAnalyzeSamples_2016_nanoAOD_sync.py
@@ -0,0 +1,51 @@
+from collections import OrderedDict as OD
+
+# file generated at 2019-07-10 17:14:20 with the following command:
+# create_dictionary.py -m python/samples/metaDict_2016_hh_sync.py -p /hdfs/local/karl/sync_ntuples/2016/nanoAODproduction/2019Jul10 -N samples_2016 -E 2016 -o python/samples -g hhAnalyzeSamples_2016_nanoAOD_sync.py -M
+
+samples_2016 = OD()
+samples_2016["/GluGluToRadionToHHTo2B2VTo2L2Nu_M-750_narrow_13TeV-madgraph-v2/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v2/MINIAODSIM"] = OD([
+  ("type",                            "mc"),
+  ("sample_category",                 "signal"),
+  ("process_name_specific",           "signal_ggf_spin0_750_hh_2b2v"),
+  ("nof_files",                       1),
+  ("nof_db_files",                    3),
+  ("nof_events",                      {
+  }),
+  ("nof_tree_events",                 144981),
+  ("nof_db_events",                   298727),
+  ("fsize_local",                     364741000), # 364.74MB, avg file size 364.74MB
+  ("fsize_db",                        13966996917), # 13.97GB, avg file size 4.66GB
+  ("use_it",                          True),
+  ("xsection",                        0.026422),
+  ("genWeight",                       True),
+  ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
+  ("has_LHE",                         True),
+  ("LHE_set",                         "LHA IDs 262000 - 262100 -> NNPDF30_lo_as_0130 PDF set, expecting 101 weights (counted 101 weights)"),
+  ("nof_reweighting",                 0),
+  ("local_paths",
+    [
+      OD([
+        ("path",      "/hdfs/local/karl/sync_ntuples/2016/nanoAODproduction/2019Jul10/signal_ggf_spin0_750_hh_2b2v"),
+        ("selection", "*"),
+        ("blacklist", []),
+      ]),
+    ]
+  ),
+  ("missing_completely",           [
+    # not computed
+  ]),
+  ("missing_from_superset",        [
+    # not computed
+  ]),
+  ("missing_hlt_paths",            [
+
+  ]),
+  ("hlt_paths",                    [
+    # not computed
+  ]),
+])
+
+samples_2016["sum_events"] = [
+]
+
---
+++ python/samples/hhAnalyzeSamples_2018_nanoAOD_sync.py
@@ -0,0 +1,55 @@
+from collections import OrderedDict as OD
+
+# file generated at 2019-07-10 17:14:59 with the following command:
+# create_dictionary.py -m python/samples/metaDict_2018_hh_sync.py -p /hdfs/local/karl/sync_ntuples/2018/nanoAODproduction/2019Jul10 -N samples_2018 -E 2018 -o python/samples -g hhAnalyzeSamples_2018_nanoAOD_sync.py -M
+
+samples_2018 = OD()
+samples_2018["/GluGluToRadionToHHTo2B2Tau_M-750_narrow_TuneCP5_PSWeights_13TeV-madgraph-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM"] = OD([
+  ("type",                            "mc"),
+  ("sample_category",                 "signal"),
+  ("process_name_specific",           "signal_ggf_spin0_750_hh_2b2t"),
+  ("nof_files",                       1),
+  ("nof_db_files",                    10),
+  ("nof_events",                      {
+  }),
+  ("nof_tree_events",                 42000),
+  ("nof_db_events",                   200000),
+  ("fsize_local",                     104118289), # 104.12MB, avg file size 104.12MB
+  ("fsize_db",                        11645634056), # 11.65GB, avg file size 1.16GB
+  ("use_it",                          True),
+  ("xsection",                        0.073056),
+  ("genWeight",                       True),
+  ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
+  ("has_LHE",                         True),
+  ("LHE_set",                         "LHA IDs 306000 - 306102 -> NNPDF31_nnlo_hessian_pdfas PDF set, expecting 103 weights (counted 103 weights)"),
+  ("nof_reweighting",                 0),
+  ("local_paths",
+    [
+      OD([
+        ("path",      "/hdfs/local/karl/sync_ntuples/2018/nanoAODproduction/2019Jul10/signal_ggf_spin0_750_hh_2b2t"),
+        ("selection", "*"),
+        ("blacklist", []),
+      ]),
+    ]
+  ),
+  ("missing_completely",           [
+    # not computed
+  ]),
+  ("missing_from_superset",        [
+    # not computed
+  ]),
+  ("missing_hlt_paths",            [
+    "HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg",
+    "HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg",
+    "HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg",
+    "HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1",
+    "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1",
+  ]),
+  ("hlt_paths",                    [
+    # not computed
+  ]),
+])
+
+samples_2018["sum_events"] = [
+]
+
