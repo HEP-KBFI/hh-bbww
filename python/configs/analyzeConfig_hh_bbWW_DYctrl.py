8597c08ef563d4fa02724c9ae9a0cc2a2074b27f
ktht <karlehataht@gmail.com>
2020-01-28 16:49:29 +0200

    Do not create dir tree for sync Ntuples if not necessary
    Addresses https://github.com/HEP-KBFI/tth-htt/issues/116

--- python/configs/analyzeConfig_hh_bbWW_DYctrl.py
+++ python/configs/analyzeConfig_hh_bbWW_DYctrl.py
@@ -300,6 +300,8 @@ class analyzeConfig_hh_bbWW_DYctrl(analyzeConfig_hh):
 
                 key_dir = getKey(process_name_or_dummy, lepton_charge_selection, lepton_selection_and_frWeight, central_or_shift_or_dummy)
                 for dir_type in [ DKEY_CFGS, DKEY_HIST, DKEY_LOGS, DKEY_RLES, DKEY_SYNC ]:
+                  if dir_type == DKEY_SYNC and not self.do_sync:
+                    continue
                   initDict(self.dirs, [ key_dir, dir_type ])
                   if dir_type in [ DKEY_CFGS, DKEY_LOGS ]:
                     self.dirs[key_dir][dir_type] = os.path.join(self.configDir, dir_type, self.channel,
@@ -317,6 +319,8 @@ class analyzeConfig_hh_bbWW_DYctrl(analyzeConfig_hh):
         else:
           self.dirs[key_dir][dir_type] = os.path.join(self.outputDir, dir_type, self.channel, subdirectory)
     for dir_type in [ DKEY_CFGS, DKEY_SCRIPTS, DKEY_HIST, DKEY_LOGS, DKEY_DCRD, DKEY_PLOT, DKEY_HADD_RT, DKEY_SYNC ]:
+      if dir_type == DKEY_SYNC and not self.do_sync:
+        continue
       initDict(self.dirs, [ dir_type ])
       if dir_type in [ DKEY_CFGS, DKEY_SCRIPTS, DKEY_LOGS, DKEY_DCRD, DKEY_PLOT, DKEY_HADD_RT ]:
         self.dirs[dir_type] = os.path.join(self.configDir, dir_type, self.channel)
--- python/configs/analyzeConfig_hh_bbww_inclusive.py
+++ python/configs/analyzeConfig_hh_bbww_inclusive.py
@@ -88,6 +88,8 @@ class analyzeConfig_hh_bbww_inclusive(analyzeConfig_hh):
             continue
           key_dir = getKey(process_name_or_dummy, central_or_shift_or_dummy)
           for dir_type in [ DKEY_CFGS, DKEY_LOGS, DKEY_RLES, DKEY_SYNC ]:
+            if dir_type == DKEY_SYNC and not self.do_sync:
+              continue
             initDict(self.dirs, [ key_dir, dir_type ])
             if dir_type in [ DKEY_CFGS, DKEY_LOGS ]:
               self.dirs[key_dir][dir_type] = os.path.join(self.configDir, dir_type, self.channel, process_name_or_dummy, central_or_shift_or_dummy)
@@ -103,6 +105,8 @@ class analyzeConfig_hh_bbww_inclusive(analyzeConfig_hh):
         else:
           self.dirs[key_dir][dir_type] = os.path.join(self.outputDir, dir_type, self.channel, subdirectory)
     for dir_type in [ DKEY_CFGS, DKEY_SCRIPTS, DKEY_LOGS, DKEY_SYNC, DKEY_HADD_RT ]:
+      if dir_type == DKEY_SYNC and not self.do_sync:
+        continue
       initDict(self.dirs, [ dir_type ])
       if dir_type in [ DKEY_CFGS, DKEY_SCRIPTS, DKEY_LOGS, DKEY_HADD_RT ]:
         self.dirs[dir_type] = os.path.join(self.configDir, dir_type, self.channel)
