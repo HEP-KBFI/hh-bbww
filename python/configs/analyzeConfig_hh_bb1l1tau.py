d95fe5e331996b06c9f8f309eb34c41a3a6e4c39
ktht <karlehataht@gmail.com>
2020-02-08 19:36:39 +0200

    Enable systematics in sidebands
    https://github.com/HEP-KBFI/tth-htt/issues/120

--- python/configs/analyzeConfig_hh_bb1l1tau.py
+++ python/configs/analyzeConfig_hh_bb1l1tau.py
@@ -148,8 +148,7 @@ class analyzeConfig_hh_bb1l1tau(analyzeConfig_hh):
   def accept_systematics(self, central_or_shift, is_mc, lepton_and_hadTau_selection, chargeSumSelection, sample_info):
     if central_or_shift != "central":
       isFR_shape_shift = (central_or_shift in self.central_or_shifts_fr)
-      if not ((lepton_and_hadTau_selection == "Fakeable" and chargeSumSelection == "OS" and isFR_shape_shift) or
-              (lepton_and_hadTau_selection == "Tight" and chargeSumSelection == "OS")):
+      if not ((lepton_and_hadTau_selection == "Fakeable" and isFR_shape_shift) or lepton_and_hadTau_selection == "Tight"):
         return False
       if isFR_shape_shift and lepton_and_hadTau_selection == "Tight":
         return False
