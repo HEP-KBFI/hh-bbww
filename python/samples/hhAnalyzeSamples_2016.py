459c22c5a00fd5140cac81ec7fc5b22d7151a2c8
ktht <karlehataht@gmail.com>
2020-04-02 17:13:26 +0300

    Load samples for ttbar systematics if they're available
    https://github.com/HEP-KBFI/tth-htt/issues/140

--- python/samples/hhAnalyzeSamples_2016.py
+++ python/samples/hhAnalyzeSamples_2016.py
@@ -1,5 +1,11 @@
 from tthAnalysis.HiggsToTauTau.samples.tthAnalyzeSamples_2016_base import samples_2016 as samples_2016_bkg
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2016_hh import samples_2016 as samples_2016_hh
 
+samples_2016_ttbar = None
+try:
+  from hhAnalysis.bbww.samples.hhAnalyzeSamples_2016_ttbar import samples_2016 as samples_2016_ttbar
+except ImportError:
+  pass
+
 from hhAnalysis.multilepton.samples.reclassifySamples import reclassifySamples
-samples_2016 = reclassifySamples(samples_2016_hh, samples_2016_bkg)
+samples_2016 = reclassifySamples(samples_2016_hh, samples_2016_bkg, samples_2016_ttbar)
--- python/samples/hhAnalyzeSamples_2018.py
+++ python/samples/hhAnalyzeSamples_2018.py
@@ -1,5 +1,11 @@
 from tthAnalysis.HiggsToTauTau.samples.tthAnalyzeSamples_2018_base import samples_2018 as samples_2018_bkg
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2018_hh import samples_2018 as samples_2018_hh
 
+samples_2018_ttbar = None
+try:
+  from hhAnalysis.bbww.samples.hhAnalyzeSamples_2018_ttbar import samples_2018 as samples_2018_ttbar
+except ImportError:
+  pass
+
 from hhAnalysis.multilepton.samples.reclassifySamples import reclassifySamples
-samples_2018 = reclassifySamples(samples_2018_hh, samples_2018_bkg)
+samples_2018 = reclassifySamples(samples_2018_hh, samples_2018_bkg, samples_2018_ttbar)
