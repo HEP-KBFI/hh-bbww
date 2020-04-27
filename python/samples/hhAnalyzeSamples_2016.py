5d479b166762ce11ea0e75aa7ee96ae5d60594c5
ktht <karlehataht@gmail.com>
2020-04-27 18:34:06 +0300

    Post-processed Ntuplesfor ttbar systematics
    https://github.com/HEP-KBFI/tth-htt/issues/140

--- python/samples/hhAnalyzeSamples_2016.py
+++ python/samples/hhAnalyzeSamples_2016.py
@@ -1,11 +1,6 @@
 from tthAnalysis.HiggsToTauTau.samples.tthAnalyzeSamples_2016_base import samples_2016 as samples_2016_bkg
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2016_hh import samples_2016 as samples_2016_hh
-
-samples_2016_ttbar = None
-try:
-  from hhAnalysis.bbww.samples.hhAnalyzeSamples_2016_ttbar import samples_2016 as samples_2016_ttbar
-except ImportError:
-  pass
+from hhAnalysis.bbww.samples.hhAnalyzeSamples_2016_ttbar import samples_2016 as samples_2016_ttbar
 
 from hhAnalysis.multilepton.samples.reclassifySamples import reclassifySamples
 samples_2016 = reclassifySamples(samples_2016_hh, samples_2016_bkg, samples_2016_ttbar)
