66987c0ba663b6775d458909d50c37692460bb89
ktht <karlehataht@gmail.com>
2020-04-28 13:16:56 +0300

    Skim ttbar samples
    https://github.com/HEP-KBFI/tth-htt/issues/140

--- python/samples/hhAnalyzeSamples_2017_preselected.py
+++ python/samples/hhAnalyzeSamples_2017_preselected.py
@@ -1,5 +1,6 @@
 from tthAnalysis.HiggsToTauTau.samples.tthAnalyzeSamples_2017_preselected_base import samples_2017 as samples_2017_bkg
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2017_hh import samples_2017 as samples_2017_hh
+from hhAnalysis.bbww.samples.hhAnalyzeSamples_2017_ttbar_preselected import samples_2017 as samples_2017_ttbar
 
 from hhAnalysis.multilepton.samples.reclassifySamples import reclassifySamples
-samples_2017 = reclassifySamples(samples_2017_hh, samples_2017_bkg)
+samples_2017 = reclassifySamples(samples_2017_hh, samples_2017_bkg, samples_2017_ttbar)
