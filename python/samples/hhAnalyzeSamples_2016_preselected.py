04f3f07db10f5cd41d11f7e2e579d4a76807b482
ktht <karlehataht@gmail.com>
2020-06-25 14:07:54 +0300

    Enable new samples; disable NLO DY; disable a 2017 TTToHadronic sample
    https://github.com/HEP-KBFI/hh-bbww/issues/9

--- python/samples/hhAnalyzeSamples_2016_preselected.py
+++ python/samples/hhAnalyzeSamples_2016_preselected.py
@@ -2,5 +2,5 @@ from tthAnalysis.HiggsToTauTau.samples.tthAnalyzeSamples_2016_preselected_base i
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2016_hh import samples_2016 as samples_2016_hh
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2016_ttbar_preselected import samples_2016 as samples_2016_ttbar
 
-from hhAnalysis.multilepton.samples.reclassifySamples import reclassifySamples
+from hhAnalysis.bbww.samples.reclassifySamples import reclassifySamples
 samples_2016 = reclassifySamples(samples_2016_hh, samples_2016_bkg, samples_2016_ttbar)
--- python/samples/hhAnalyzeSamples_2017_preselected.py
+++ python/samples/hhAnalyzeSamples_2017_preselected.py
@@ -2,5 +2,5 @@ from tthAnalysis.HiggsToTauTau.samples.tthAnalyzeSamples_2017_preselected_base i
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2017_hh import samples_2017 as samples_2017_hh
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2017_ttbar_preselected import samples_2017 as samples_2017_ttbar
 
-from hhAnalysis.multilepton.samples.reclassifySamples import reclassifySamples
+from hhAnalysis.bbww.samples.reclassifySamples import reclassifySamples
 samples_2017 = reclassifySamples(samples_2017_hh, samples_2017_bkg, samples_2017_ttbar)
