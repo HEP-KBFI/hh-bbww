b652176daa1441f9779357dab811e8b53b99aee0
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2020-07-20 21:34:40 +0300

    adding the samples we want to train on - W and DY are always added

--- python/samples/hhAnalyzeSamples_2016_BDT.py
+++ python/samples/hhAnalyzeSamples_2016_BDT.py
@@ -30,6 +30,17 @@ bdt_samples = [
   "signal_ggf_nonresonant_node_11_hh_2b2v_sl",
   "signal_ggf_nonresonant_node_12_hh_2b2v_sl",
   "TTTo2L2Nu"
+  "signal_ggf_spin0_300_hh_2b2v_sl",
+  "signal_ggf_spin0_400_hh_2b2v_sl",
+  "signal_ggf_spin0_500_hh_2b2v_sl",
+  "signal_ggf_spin0_900_hh_2b2v_sl",
+  "signal_ggf_spin0_1000_hh_2b2v_sl",
+  "signal_ggf_spin0_300_hh_2b2v",
+  "signal_ggf_spin0_400_hh_2b2v",
+  "signal_ggf_spin0_500_hh_2b2v",
+  "signal_ggf_spin0_900_hh_2b2v",
+  "signal_ggf_spin0_1000_hh_2b2v",
+  "TTToSemiLeptonic"
 ]
 
 for sample_name, sample_info in samples_2016.items():
