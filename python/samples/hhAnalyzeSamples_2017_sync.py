e2af60fe4e1fa743109aad4c5ba7afa1d5a140f0
ktht <karlehataht@gmail.com>
2020-10-24 22:19:32 +0300

    Update sync Ntuples

--- python/samples/hhAnalyzeSamples_2017_sync.py
+++ python/samples/hhAnalyzeSamples_2017_sync.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict as OD
 
-# file generated at 2020-04-02 21:02:04 with the following command:
-# create_dictionary.py -m python/samples/metaDict_2017_hh_sync.py -p /hdfs/local/karl/ttHNtupleProduction/2017/2020Apr02_woPresel_nonNom_hh_bbww_sync/ntuples -N samples_2017 -E 2017 -o python/samples -g hhAnalyzeSamples_2017_sync.py -M
+# file generated at 2020-10-24 22:12:49 with the following command:
+# create_dictionary.py -m python/samples/metaDict_2017_hh_sync.py -p /hdfs/local/karl/ttHNtupleProduction/2017/2020Oct24_woPresel_nonNom_hh_bbww_sync/ntuples -N samples_2017 -E 2017 -o python/samples -g hhAnalyzeSamples_2017_sync.py -M
 
 samples_2017 = OD()
 samples_2017["/GluGluToRadionToHHTo2B2VTo2L2Nu_M-750_narrow_13TeV-madgraph_correctedcfg/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"] = OD([
@@ -11,16 +11,21 @@ samples_2017["/GluGluToRadionToHHTo2B2VTo2L2Nu_M-750_narrow_13TeV-madgraph_corre
   ("nof_files",                       1),
   ("nof_db_files",                    11),
   ("nof_events",                      {
-    'Count'                                                                          : [        52000, ],
-    'CountWeighted'                                                                  : [        51992,        51990,        51983, ],
-    'CountWeightedLHEEnvelope'                                                       : [        51992,        51992, ],
-    'CountWeightedL1PrefireNom'                                                      : [        50028,        50025,        50025, ],
-    'CountWeightedL1Prefire'                                                         : [        50028,        49579,        50472, ],
-    'CountWeightedLHEEnvelopeL1PrefireNom'                                           : [        50028,        50028, ],
+    'Count'                                                                          : [ 52000, ],
+    'CountWeighted'                                                                  : [ 5.19923398e+04, 5.19904453e+04, 5.19829805e+04, ],
+    'CountWeightedLHEEnvelope'                                                       : [ 5.19923398e+04, 5.19923398e+04, ],
+    'CountWeightedFull'                                                              : [ 5.19923398e+04, 5.19904453e+04, 5.19829805e+04, ],
+    'CountWeightedFullLHEEnvelope'                                                   : [ 5.19923398e+04, 5.19923398e+04, ],
+    'CountWeightedL1PrefireNom'                                                      : [ 5.00281172e+04, 5.00254805e+04, 5.00253008e+04, ],
+    'CountWeightedL1Prefire'                                                         : [ 5.00281172e+04, 4.95789570e+04, 5.04720234e+04, ],
+    'CountWeightedLHEEnvelopeL1PrefireNom'                                           : [ 5.00281172e+04, 5.00281172e+04, ],
+    'CountWeightedFullL1PrefireNom'                                                  : [ 5.00281172e+04, 5.00254805e+04, 5.00253008e+04, ],
+    'CountWeightedFullL1Prefire'                                                     : [ 5.00281172e+04, 4.95789570e+04, 5.04720234e+04, ],
+    'CountWeightedFullLHEEnvelopeL1PrefireNom'                                       : [ 5.00281172e+04, 5.00281172e+04, ],
   }),
   ("nof_tree_events",                 52000),
   ("nof_db_events",                   200000),
-  ("fsize_local",                     244083118), # 244.08MB, avg file size 244.08MB
+  ("fsize_local",                     244090247), # 244.09MB, avg file size 244.09MB
   ("fsize_db",                        11931037531), # 11.93GB, avg file size 1.08GB
   ("use_it",                          True),
   ("xsection",                        0.026422),
@@ -33,7 +38,7 @@ samples_2017["/GluGluToRadionToHHTo2B2VTo2L2Nu_M-750_narrow_13TeV-madgraph_corre
   ("local_paths",
     [
       OD([
-        ("path",      "/hdfs/local/karl/ttHNtupleProduction/2017/2020Apr02_woPresel_nonNom_hh_bbww_sync/ntuples/signal_ggf_spin0_750_hh_2b2v"),
+        ("path",      "/hdfs/local/karl/ttHNtupleProduction/2017/2020Oct24_woPresel_nonNom_hh_bbww_sync/ntuples/signal_ggf_spin0_750_hh_2b2v"),
         ("selection", "*"),
         ("blacklist", []),
       ]),
