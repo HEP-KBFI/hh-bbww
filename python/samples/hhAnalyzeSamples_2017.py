ea0a3cccb88f28f64384ba3346831a369b21a831
Christian Veelken <christian.veelken@cern.ch>
2020-11-22 12:20:03 +0200

    split ttbar and single top into "TT" and "ST" consistently for all 3 years

--- python/samples/hhAnalyzeSamples_2017.py
+++ python/samples/hhAnalyzeSamples_2017.py
@@ -3,4 +3,4 @@ from hhAnalysis.bbww.samples.hhAnalyzeSamples_2017_hh import samples_2017 as sam
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2017_ttbar import samples_2017 as samples_2017_ttbar
 
 from hhAnalysis.bbww.samples.reclassifySamples import reclassifySamples
-samples_2017 = reclassifySamples(samples_2017_hh, samples_2017_bkg, samples_2017_ttbar)
+samples_2017 = reclassifySamples(samples_2017_hh, samples_2017_bkg, samples_2017_ttbar, separate_ST = True)
