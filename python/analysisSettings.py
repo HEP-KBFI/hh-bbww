bb3674c74b1c1439075f225c8d4cb72cab071221
ktht <karlehataht@gmail.com>
2020-12-17 16:39:21 +0200

    Remove custom modification of PS and LHE scale systematics
    https://github.com/HEP-KBFI/hh-bbww/issues/24

--- python/analysisSettings.py
+++ python/analysisSettings.py
@@ -1,14 +1,10 @@
 from tthAnalysis.HiggsToTauTau.analysisSettings import systematics
 
 class systematics_bbww(systematics):
-  systematics.lhe_bbww = systematics.LHE().env_up + systematics.LHE().env_down
-  systematics.partonShower_bbww = systematics.PartonShower().isr_up + systematics.PartonShower().isr_down + \
-                                  systematics.PartonShower().fsr_up + systematics.PartonShower().fsr_down
-
   an_full_hh_bbww = systematics.central + systematics.JES + systematics.JER + systematics.leptonIDSF + \
                     systematics.UnclusteredEn + systematics.btag + systematics.FRe_shape + systematics.FRm_shape + \
-                    systematics.lhe_bbww + systematics.triggerSF + systematics.PU + systematics.DYMCNormScaleFactors + \
-                    systematics.L1PreFiring + systematics.partonShower_bbww + systematics.ttbar + systematics.AK8 + \
+                    systematics.lhe + systematics.triggerSF + systematics.PU + systematics.DYMCNormScaleFactors + \
+                    systematics.L1PreFiring + systematics.partonShower + systematics.ttbar + systematics.AK8 + \
                     systematics.pileupJetID + systematics.topPtReweighting + systematics.leptonIDSF_hh_recomp + \
                     systematics.mcClosure
   an_opts_hh_bbww = [
@@ -16,10 +12,10 @@ class systematics_bbww(systematics):
     "DYMCNormScaleFactors", "L1PreFiring", "partonShower", "ttbar", "AK8", "pileupJetID", "topPtReweighting",
     "leptonIDSF_hh_recomp", systematics.mcClosure_str,
   ]
-  an_internal_hh_bbww = systematics.central + systematics.leptonIDSF + systematics.btag + systematics.lhe_bbww + \
+  an_internal_hh_bbww = systematics.central + systematics.leptonIDSF + systematics.btag + systematics.lhe + \
                         systematics.triggerSF + systematics.PU + systematics.L1PreFiring + systematics.FRe_shape + \
                         systematics.FRm_shape + systematics.DYMCNormScaleFactors + systematics.topPtReweighting + \
-                        systematics.partonShower_bbww + systematics.leptonIDSF_hh_recomp + systematics.pileupJetID
+                        systematics.partonShower + systematics.leptonIDSF_hh_recomp + systematics.pileupJetID
 
 class systematics_bbww_dl(systematics_bbww):
   pass
