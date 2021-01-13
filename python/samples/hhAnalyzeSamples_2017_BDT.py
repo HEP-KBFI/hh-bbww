94fa3e4d64d2171e1d7211136d37cf36f34b7e2f
saswatinandan <saswati.nandan@cern.ch>
2021-01-13 16:54:00 +0200

    adding ST for bdt training

--- python/samples/hhAnalyzeSamples_2017_BDT.py
+++ python/samples/hhAnalyzeSamples_2017_BDT.py
@@ -1,29 +1,21 @@
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2017 import samples_2017
 
 bdt_samples = [
-  "signal_ggf_nonresonant_node_sm_hh_2b2v",
-  "signal_ggf_nonresonant_node_2_hh_2b2v",
-  "signal_ggf_nonresonant_node_3_hh_2b2v",
-  "signal_ggf_nonresonant_node_7_hh_2b2v",
-  "signal_ggf_nonresonant_node_9_hh_2b2v",
-  "signal_ggf_nonresonant_node_12_hh_2b2v",
-  "signal_ggf_nonresonant_node_sm_hh_2b2v_sl",
   "TTTo2L2Nu",
   "TTTo2L2Nu_PSweights",
   "TTToSemiLeptonic",
   "TTToSemiLeptonic_PSweights",
-  "signal_ggf_spin0_300_hh_2b2v_sl",
-  "signal_ggf_spin0_400_hh_2b2v_sl",
-  "signal_ggf_spin0_500_hh_2b2v_sl",
-  "signal_ggf_spin0_900_hh_2b2v_sl",
-  "signal_ggf_spin0_1000_hh_2b2v_sl",
-  "signal_ggf_spin0_300_hh_2b2v",
-  "signal_ggf_spin0_400_hh_2b2v",
-  "signal_ggf_spin0_500_hh_2b2v",
-  "signal_ggf_spin0_900_hh_2b2v",
-  "signal_ggf_spin0_1000_hh_2b2v"
+  "ST_s-channel_4f_leptonDecays",
+  "ST_tWll",
+  "ST_t-channel_antitop_4f_inclusiveDecays",
+  "ST_s-channel_4f_leptonDecays_PSweights",
+  "ST_tW_antitop_5f_inclusiveDecays_ext1",
+  "ST_tW_top_5f_inclusiveDecays_ext1",
+  "ST_t-channel_top_4f_inclusiveDecays",
+  "ST_t-channel_antitop_4f_inclusiveDecays_PSweights"
 ]
 
 for sample_name, sample_info in samples_2017.items():
   if sample_name == 'sum_events': continue
-  sample_info["use_it"] = sample_info["process_name_specific"] in bdt_samples
+  sample_info["use_it"] = sample_info["process_name_specific"] in bdt_samples or\
+                          'signal' in sample_info["process_name_specific"]
--- python/samples/hhAnalyzeSamples_2018_BDT.py
+++ python/samples/hhAnalyzeSamples_2018_BDT.py
@@ -1,19 +1,21 @@
 from hhAnalysis.bbww.samples.hhAnalyzeSamples_2018 import samples_2018
 
 bdt_samples = [
-  "signal_ggf_nonresonant_node_sm_hh_2b2v",
-  "signal_ggf_nonresonant_node_2_hh_2b2v",
-  "signal_ggf_nonresonant_node_3_hh_2b2v",
-  "signal_ggf_nonresonant_node_7_hh_2b2v",
-  "signal_ggf_nonresonant_node_9_hh_2b2v",
-  "signal_ggf_nonresonant_node_12_hh_2b2v",
-  "signal_ggf_nonresonant_node_sm_hh_2b2v_sl",
   "TTTo2L2Nu",
   "TTTo2L2Nu_PSweights",
   "TTToSemiLeptonic",
-  "TTToSemiLeptonic_PSweights"
+  "TTToSemiLeptonic_PSweights",
+  "ST_s-channel_4f_leptonDecays",
+  "ST_tWll",
+  "ST_t-channel_antitop_4f_inclusiveDecays",
+  "ST_s-channel_4f_leptonDecays_PSweights",
+  "ST_tW_antitop_5f_inclusiveDecays_ext1",
+  "ST_tW_top_5f_inclusiveDecays_ext1",
+  "ST_t-channel_top_4f_inclusiveDecays",
+  "ST_t-channel_antitop_4f_inclusiveDecays_PSweights"
 ]
 
 for sample_name, sample_info in samples_2018.items():
   if sample_name == 'sum_events': continue
-  sample_info["use_it"] = sample_info["process_name_specific"] in bdt_samples
+  sample_info["use_it"] = sample_info["process_name_specific"] in bdt_samples or\
+                          'signal' in sample_info["process_name_specific"]
