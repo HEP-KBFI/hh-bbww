f5e4b37484cc1da5f0a9efdd44dbb6d6d30b0080
Christian Veelken <christian.veelken@cern.ch>
2021-01-15 11:09:00 +0200

    removed matching vs "CHH" string

--- test/hhAnalyzeRun_hh_bb2l.py
+++ test/hhAnalyzeRun_hh_bb2l.py
@@ -190,8 +190,6 @@ else:
 for sample_name, sample_info in samples.items():
   if sample_name == 'sum_events':
     continue
-  if "CHH" in sample_name:
-    sample_info["use_it"] = False
   if 'nonres' in fill_spin and 'spin' in sample_info['process_name_specific']:
     sample_info["use_it"] = False
   if 'spin0' in fill_spin and 'nonres' in sample_info['process_name_specific']:
