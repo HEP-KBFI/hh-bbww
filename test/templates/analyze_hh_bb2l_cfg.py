60007e1eb22edf7ab52e156b353aebe3b314f906
ktht <karlehataht@gmail.com>
2021-04-18 19:06:04 +0300

    Recategorize samples based on gen photon filter flag in cfg base class
    Also, added missing branch names to the python cfg template files. https://github.com/HEP-KBFI/hh-multilepton/issues/36

--- test/templates/analyze_hh_bb2l_cfg.py
+++ test/templates/analyze_hh_bb2l_cfg.py
@@ -91,6 +91,8 @@ process.analyze_hh_bb2l = cms.PSet(
     branchName_genLeptons = cms.string('GenLep'),
     branchName_genHadTaus = cms.string('GenVisTau'),
     branchName_genPhotons = cms.string('GenPhoton'),
+    branchName_genProxyPhotons = cms.string('GenPhotonCandidate'),
+    branchName_genFromHardProcess = cms.string('GenIsHardProcess'),
     branchName_genJets = cms.string('GenJet'),
     branchName_genHiggses = cms.string('GenHiggs'),
 
