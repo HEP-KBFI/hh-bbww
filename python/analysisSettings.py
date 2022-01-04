0ba6dfacb0f99833d6c6c81a3b972eb5e68cc5cb
ktht <karlehataht@gmail.com>
2022-01-05 01:02:00 +0200

    Add PDF member systematics in the comments

--- python/analysisSettings.py
+++ python/analysisSettings.py
@@ -6,11 +6,11 @@ class systematics_bbww(systematics):
                     systematics.lhe + systematics.triggerSF + systematics.PU + systematics.DYMCNormScaleFactors + \
                     systematics.L1PreFiring + systematics.partonShower + systematics.AK8 + \
                     systematics.pileupJetID + systematics.topPtReweighting + systematics.leptonIDSF_hh_recomp + \
-                    systematics.mcClosure + systematics.ttbar + systematics.pdf
+                    systematics.mcClosure + systematics.ttbar + systematics.pdf # + systematics.pdf_mem
   an_opts_hh_bbww = [
     "central", "JES", "JER", "leptonIDSF", "UnclusteredEn", "btag", "FRe_shape", "FRm_shape", "lhe", "triggerSF", "PU",
     "DYMCNormScaleFactors", "L1PreFiring", "partonShower", "AK8", "pileupJetID", "topPtReweighting",
-    "leptonIDSF_hh_recomp", systematics.mcClosure_str, "ttbar", "pdf",
+    "leptonIDSF_hh_recomp", systematics.mcClosure_str, "ttbar", "pdf", # "pdf_mem"
   ]
   an_internal_hh_bbww = systematics.central + systematics.leptonIDSF + systematics.btag + systematics.lhe + \
                         systematics.triggerSF + systematics.PU + systematics.L1PreFiring + systematics.FRe_shape + \
